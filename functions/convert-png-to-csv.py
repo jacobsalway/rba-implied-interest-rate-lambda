3309de04cc3e51ac5183d46b061fc3f09207e7b6
Jacob Salway <7677151+xorana@users.noreply.github.com>
2020-12-25 18:27:51 +1100

    Update OCR and preprocessing

--- functions/convert-png-to-csv.py
+++ functions/convert-png-to-csv.py
@@ -1,5 +1,5 @@
-from PIL import Image
-import PIL.ImageOps
+import cv2
+import numpy
 import pytesseract
 from datetime import datetime
 import boto3
@@ -25,32 +25,7 @@ def download_record(s3_client, record):
 
     return download_path
 
-def crop_to_text(image):
-    # crop to interest rate text section
-    w, h = image.size
-    image = image.crop((137, 844, w, h))
-
-    return image
-
-def strip_borders(image):
-    # get rid of one pixel borders that might mess up OCR
-    w, h = image.size
-    for x in range(w):
-        for y in range(h):
-            v = image.getpixel((x, y))
-            a = image.getpixel((x, y-1)) if y-1 >= 0 else 0
-            b = image.getpixel((x+1, y)) if x+1 < w else 0
-            c = image.getpixel((x+1, y+1)) if x+1 < w and y+1 < h else 0
-            d = image.getpixel((x-1, y)) if x-1 >= 0 else 0
-
-            if v == 255 and ((a,c) == (0, 0) or (b,d) == (0,0)):
-                image.putpixel((x,y), 0)
-
-    return image
-
-def test_and_convert(text):
-    lines = text.strip().split('\n')
-
+def test_and_convert(lines):
     # make sure only two lines of recognised text
     if len(lines) != 2:
         raise Exception('OCR failed')
@@ -59,8 +34,18 @@ def test_and_convert(text):
     rates = lines[1].split(' ')
 
     # make sure OCR did not miss any months or add any extras
-    if len(months) != 18 or len(rates) != 18:
+    if len(months) != len(rates):
+        print(months)
+        print(rates)
         raise Exception('OCR missed text')
+
+    for i in range(len(months)):
+        if '-' not in months[i]:
+            months[i] = '{}-{}'.format(months[i][:3], months[i][3:])
+
+    for i in range(len(rates)):
+        if '.' not in rates[i]:
+            rates[i] = '{}.{}'.format(rates[i][0], rates[i][1:])
     
     # test the format of each recognised text row and convert for writing to csv
     # Jan-20, Feb-20 etc. for months
@@ -80,18 +65,35 @@ def lambda_handler(event, context):
         image_path = download_record(s3_client, record)
         file_name = get_file_name(record)
 
-        image = Image.open(image_path)
+        image = cv2.imread(image_path, 0)
 
-        image = crop_to_text(image)
-        image = strip_borders(image)
+        image = cv2.bitwise_not(image)
 
-        # invert so text is black on white
-        image = PIL.ImageOps.invert(image)
+        w, h = tuple(image.shape[1::-1])
+        x, y = 0, int(h * 0.9)
 
-        # do OCR to convert image to text
-        text = pytesseract.image_to_string(image)
+        image = image[y:h, 100:]
 
-        months, rates = test_and_convert(text)
+        mask = image.mean(axis=1) < 50
+        image[mask, :] = 255
+        mask = image.mean(axis=0) < 125
+        image[:, mask] = 255
+
+        image = cv2.bilateralFilter(image, 15, 75, 75)
+
+        # image = cv2.resize(image, None, fx = 2, fy = 2, interpolation = cv2.INTER_LINEAR)
+
+        text = pytesseract.image_to_string(image, lang='eng', config='--oem 2 --psm 6')
+
+        rates, index = text.strip().split('\n')[-2:], 0
+        for i, c in enumerate(rates[1]):
+            if c.isdigit():
+                index = i
+                break
+
+        rates[1] = rates[1][index:]
+
+        months, rates = test_and_convert(rates)
 
         file_date = file_name.split('.')[0]
         final_csv = [[file_date, i, j] for i, j in zip(months, rates)]
